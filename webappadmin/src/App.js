98b464dd111359906c84777e869cfa7504617df7
Akashprem <akashpremrajan@gmail.com>
2020-02-17 23:08:55 +0530

    Login

--- webappadmin/src/App.js
+++ webappadmin/src/App.js
@@ -2,6 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this)
+    this.handleChange = this.handleChange.bind(this)
+    this.state = {u: '', p: ''}
+  }
+
+  handleChange(event) {
+    if (event.target.name==="username")
+      this.setState({u: event.target.value})
+    if (event.target.name==="password")
+      this.setState({p: event.target.value})
+  }
+
+  handleSubmit(event) {
+    event.preventDefault()
+
+    fetch('http://localhost:5000/', {
+      headers: {'Content-Type': 'application/x-www-form-urlencoded'},
+      method: 'POST',
+      body: JSON.stringify(this.state),
+    })
+    .then((response) => console.log(response.data))
+    .catch((err) => console.log(err))
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,30 +36,27 @@ class App extends Component {
             <div className="Form__Title">
               Administrative Login
             </div>
-            <br />
-            <br />
-            <div className="Form">
+            <br /> <br />
+            <form className="Form" onSubmit={this.handleSubmit}>
               <div className="Username">
               Username: 
-              &nbsp;
-              &nbsp;
-              &nbsp;
-              <input type="text" name="username" />
+              &nbsp; &nbsp; &nbsp;
+              <input type="text" name="username" value={this.state.u} onChange={this.handleChange} />
               </div>
               <br />
               <div className="Password">
               Password: 
-              &nbsp;
-              &nbsp;
-              &nbsp;
-              <input type="text" name="password" />
+              &nbsp; &nbsp; &nbsp;
+              <input type="password" name="password" value={this.state.p} onChange={this.handleChange} />
               </div>
-            </div>
+              <br /> <br /> <br />
+              <button className="submit" type="submit"> Login </button>
+            </form>
           </div>
         </div>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
